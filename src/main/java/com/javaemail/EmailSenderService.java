a1a14fa7cd190fbf68619f069c6674f3501ba6b8
Made Manik Datu Yasa <manikyasa@gmail.com>
2024-06-26 06:47:46 +0700

    feat: add email with attachment

--- src/main/java/com/javaemail/EmailSenderService.java
+++ src/main/java/com/javaemail/EmailSenderService.java
@@ -1,9 +1,15 @@
 package com.javaemail;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 public class EmailSenderService {
@@ -23,5 +29,24 @@ public class EmailSenderService {
         mailSender.send(message);
 
         System.out.println("Mail Send Successfully");
+    }
+
+    public void sendEmailWithAttachment(String toEmail,
+                                        String subject,
+                                        String body,
+                                        String attachment) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+        mimeMessageHelper.setFrom("blimanikyasa@gmail.com");
+        mimeMessageHelper.setTo(toEmail);
+        mimeMessageHelper.setText(body);
+        mimeMessageHelper.setSubject(subject);
+
+        FileSystemResource fileSystemResource =
+                new FileSystemResource(new File(attachment));
+        mimeMessageHelper.addAttachment(fileSystemResource.getFilename(),fileSystemResource);
+        mailSender.send(mimeMessage);
+        System.out.println("Mail with attachments sent successfully");
     }
 }
--- src/main/java/com/javaemail/JavaEmailDemoApplication.java
+++ src/main/java/com/javaemail/JavaEmailDemoApplication.java
@@ -1,5 +1,6 @@
 package com.javaemail;
 
+import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,9 +17,19 @@ public class JavaEmailDemoApplication {
         SpringApplication.run(JavaEmailDemoApplication.class, args);
     }
 
-	@EventListener(ApplicationReadyEvent.class)
-    public void sendMail() {
-        senderService.sendEmail("manik.yasa@gmail.com",
-                "This is subject demo", "This is body demo");
+    //open if want send simple email without attachmentss
+//	@EventListener(ApplicationReadyEvent.class)
+//    public void sendMail() {
+//        senderService.sendEmail("manik.yasa@gmail.com",
+//                "This is subject demo", "This is body demo");
+//    }
+
+    //open if want send simple email with attachmentss
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendMailWithAttachment() throws MessagingException {
+        senderService.sendEmailWithAttachment("manik.yasa@gmail.com",
+                "This is subject demo with attachment",
+                "This is body demo with attachment",
+                "C://opt//MPNG2BULK//REPORT//BPN//20240625//27//028860330773039981827862859290729.pdf");
     }
 }
